cd5ba9f6bd20d0aa8d2352f5cd24a2ee8803bdfe
Katka Marcincakova <katulik137@gmail.com>
2023-08-08 22:57:33 +0200

    maybe?

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://katkamarcincakova.github.io/astro-tina-test/',
-  integrations: [
-    tailwind({
-      // Example: Disable injecting a basic `base.css` import on every page.
-      // Useful if you need to define and/or import your own custom `base.css`.
-      applyBaseStyles: false,
-    }),
-  ],
+  bae: '/src',
+  integrations: [tailwind()],
 });
