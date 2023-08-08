ff1deae3bdb392533957bee424043a0923b8d70d
Katka Marcincakova <katulik137@gmail.com>
2023-08-08 23:05:02 +0200

    maybe?

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://katkamarcincakova.github.io/astro-tina-test/',
-  bae: '/src',
+  base: '/src',
   integrations: [tailwind()],
 });
