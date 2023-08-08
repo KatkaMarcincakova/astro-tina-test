69791b0ef5e7986d7c381aee2d828e57dd291b6c
Katka Marcincakova <katulik137@gmail.com>
2023-08-08 21:55:16 +0200

    astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,4 @@
 import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-
-import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
-export default defineConfig({
-  site: 'https://katkamarcincakova.github.io/astro-tina-test',
-  base: '/src',
-  integrations: [mdx(), sitemap(), tailwind()]
-});
+export default defineConfig({});
--- tina/__generated__/client.ts
+++
@@ -1,5 +0,0 @@
-import { createClient } from "tinacms/dist/client";
-import { queries } from "./types";
-export const client = createClient({ url: 'http://localhost:4001/graphql', token: '08f6515a40d3341fc73ee12f27d430a5a843d433', queries });
-export default client;
-  
