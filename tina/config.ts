3299724734f7bd06b68f87e611235f7abd501220
Katka Marcincakova <katulik137@gmail.com>
2023-08-09 18:00:43 +0200

    added tokens

--- tina/config.ts
+++ tina/config.ts
@@ -5,8 +5,8 @@ const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main";
 
 export default defineConfig({
   branch,
-  clientId: null, // Get this from tina.io
-  token: null, // Get this from tina.io
+  clientId: "5ef01d6a-1902-4444-b96c-688d9c4beae5", // Get this from tina.io
+  token: "08f6515a40d3341fc73ee12f27d430a5a843d433", // Get this from tina.io
 
   build: {
     outputFolder: "admin",
