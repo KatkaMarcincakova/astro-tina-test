22d5ad0a4e7f46c02805ec8c24d9bb9bd13b9e59
Katka Marcincakova <katulik137@gmail.com>
2023-08-09 18:18:18 +0200

    hgueroi;

--- tina/__generated__/config.prebuild.jsx
+++ tina/__generated__/config.prebuild.jsx
@@ -1,22 +1,20 @@
+// tina/config.ts
 import { defineConfig } from "tinacms";
-
-// Your hosting provider likely exposes this as an environment variable
-const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "master";
-
-export default defineConfig({
+var branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "master";
+var config_default = defineConfig({
   branch,
-  clientId: "5ef01d6a-1902-4444-b96c-688d9c4beae5", // Get this from tina.io
+  clientId: "5ef01d6a-1902-4444-b96c-688d9c4beae5",
+  // Get this from tina.io
   token: "08f6515a40d3341fc73ee12f27d430a5a843d433",
-
   build: {
     outputFolder: "admin",
-    publicFolder: "public",
+    publicFolder: "public"
   },
   media: {
     tina: {
       mediaRoot: "",
-      publicFolder: "public",
-    },
+      publicFolder: "public"
+    }
   },
   schema: {
     collections: [
@@ -30,16 +28,19 @@ export default defineConfig({
             name: "title",
             label: "Title",
             isTitle: true,
-            required: true,
+            required: true
           },
           {
             type: "rich-text",
             name: "body",
             label: "Body",
-            isBody: true,
-          },
-        ],
-      },
-    ],
-  },
+            isBody: true
+          }
+        ]
+      }
+    ]
+  }
 });
+export {
+  config_default as default
+};
