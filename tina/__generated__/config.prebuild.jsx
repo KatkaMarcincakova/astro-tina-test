24e74b2b8498a105a8a45245e5dbfb2f6741e4e3
Katka Marcincakova <katulik137@gmail.com>
2023-08-11 15:30:27 +0200

    test

--- tina/__generated__/config.prebuild.jsx
+++ tina/__generated__/config.prebuild.jsx
@@ -6,6 +6,7 @@ var config_default = defineConfig({
   clientId: "5ef01d6a-1902-4444-b96c-688d9c4beae5",
   // Get this from tina.io
   token: "08f6515a40d3341fc73ee12f27d430a5a843d433",
+  // Get this from tina.io
   build: {
     outputFolder: "admin",
     publicFolder: "public"
